72fe1811ac1ce4ca20b1830cbb1078dd971d4fe1
Piotr Bujakowski <piotrb2727@gmail.com>
2014-12-04 00:25:18 +0100

    repaired parsing arguments

--- server/main.cpp
+++ server/main.cpp
@@ -7,23 +7,24 @@ int main(int argc, char* argv[])
 
 	for(int i = 0; i < argc; ++i)
 	{
-		switch(argv[i])
+		if(argv[i] == "-c")
 		{
-			case "-c":
-				if(i+1 <= argc)
-				{
-					std::cerr << "filename can't be empty" << std::endl;
-					return 0;
-				}
-				config_file = std::string(argv[i + 1]);
-				i++;
-				break;
-			case "--help":
-				//todo write help
-				std::cerr << "not wried yet" << std::endl;
-				break;
-			default:
-				std::cerr << "argument " << argv[i] << " is wrong" << std::endl;
+			if(i+1 >= argc)
+			{
+				std::cerr << "filename can't be empty" << std::endl;
+				return 0;
+			}
+			config_file = std::string(argv[i + 1]);
+			i++;
+		}
+		else if(argv[i] == "--help")
+		{
+			//todo write help
+			std::cerr << "not writed yet" << std::endl;
+		}
+		else
+		{
+			std::cerr << "argument " << argv[i] << " is wrong" << std::endl;
 		}
 	}
 
